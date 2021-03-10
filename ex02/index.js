c03014a639f76364ff136c5661d162ce14d65d11
Admira11 <80249903+Admira11@users.noreply.github.com>
2021-03-11 00:59:33 +0100

    Add files via upload

---
+++ ex02/index.js
@@ -0,0 +1,14 @@
+function main(){
+    //Only change code below this line
+    
+    /* document.write ("This is a multi-line comment");
+     document.write ("This is a multi-line comment");
+     document.write ("This is a multi-line comment");
+     document.write ("This is a multi-line comment");
+     document.write ("This is a multi-line comment");
+     */
+
+     // Only change code above this line
+}
+console.log(main());
+module.exports =main;
