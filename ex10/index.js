d44e684eaeea2cf462ead7cfc1bf4a39c897d443
Admira11 <80249903+Admira11@users.noreply.github.com>
2021-03-11 01:53:53 +0100

    Add files via upload

---
+++ ex10/index.js
@@ -0,0 +1,11 @@
+function main(numOne, numTwo){
+    // Only change code below this line
+    var numOne;
+    var numTwo;
+    var product = numOne * numTwo;
+    // Only change code above this line
+    return product;
+}
+console.log(main(15, 8));
+
+module.exports = main;
