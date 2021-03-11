0b7292c19a06f3f61105e5f8f5e15776d2700930
Admira11 <80249903+Admira11@users.noreply.github.com>
2021-03-11 01:01:29 +0100

    Add files via upload

---
+++ ex05/index.js
@@ -0,0 +1,9 @@
+function main(){
+    // Only change the code below this line
+    var x =8 ;
+    // Only change the code above this line
+
+    return x;
+}
+console.log(main());
+module.exports =main;
